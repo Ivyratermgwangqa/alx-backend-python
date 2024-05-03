3f3ec32c05cb2254bc2a60a87039a61f1adcb6e9
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-03 05:38:12 +0200

    Update 3-to_str.py

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -3,6 +3,7 @@
 Module for a function that converts a float to its string representation.
 """
 
+
 def to_str(n: float) -> str:
     """Convert a float to its string representation."""
     return str(n)
