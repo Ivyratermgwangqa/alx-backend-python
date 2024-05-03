875622749b8206781333995cfea3f544befd681f
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-03 05:42:25 +0200

    Update 102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """
-Module for a function that returns a list with elements from a tuple repeated a given number of times.
+Module for a function that returns a list with elements.
 """
 
 from typing import Tuple, List
 
+
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    """Return a list with each item in the tuple repeated a given number of times."""
+    """Return a list with each item in the tuple repeated a given number."""
     return [
         item 
         for item in lst
