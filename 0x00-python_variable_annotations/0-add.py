5812b5f80d76d961ae87298eacb4a1d9392c6825
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-03 05:26:06 +0200

    Update 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Module for a function that adds two float numbers.
+"""
+
+def add(a: float, b: float) -> float:
+    """Return the sum of two float numbers."""
+    return a + b
