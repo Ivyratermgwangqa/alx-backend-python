7b2dcc72a13a16e51c490c4b786eed1c77530702
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-03 05:36:33 +0200

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """
-Module for a function that returns a tuple with a string and the square of an int/float.
+Module for a function that returns a tuple with a string .
 """
 
 from typing import Tuple, Union
+
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return a tuple with a string and the square of the given int/float."""
