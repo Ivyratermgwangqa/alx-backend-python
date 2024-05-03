dac10d294e310a36ac758bd7aefa6a68f8d668cd
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-03 05:37:03 +0200

    Update 6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """
-Module for a function that returns the sum of a list containing integers and floats.
+Module for a function that returns the sum of a list.
 """
 
 from typing import List, Union
+
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Return the sum of a list containing integers and floats."""
