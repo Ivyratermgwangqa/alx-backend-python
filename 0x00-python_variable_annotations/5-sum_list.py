498c6a3cfe39a585a9fb52d4fadb0132d43f6301
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-03 05:37:23 +0200

    Update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,6 +5,7 @@ Module for a function that returns the sum of a list of floats.
 
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """Return the sum of a list of floats."""
     return sum(input_list)
