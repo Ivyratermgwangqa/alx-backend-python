14326d1c960f1db20b2507fd5a6ec9b87f855171
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-03 05:28:58 +0200

    Update 101-safely_get_value.py

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module for a function that safely retrieves a value from a dictionary, with a default fallback.
+"""
+
+from typing import Mapping, TypeVar, Union, Any
+
+T = TypeVar("T")
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """Return the value for a key in a dictionary, or the default value if not found."""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
