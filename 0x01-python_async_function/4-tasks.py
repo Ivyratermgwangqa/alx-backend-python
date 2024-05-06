ad0474962a2909735f71c50c6f6f139eabb5fde6
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-07 01:58:33 +0200

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,7 +8,7 @@ from 3-tasks import task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Asynchronously spawn `n` instances of `task_wait_random` and return the delays in ascending order.
+    Asynchronously spawn `n` instances of `task_wait_random`.
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
