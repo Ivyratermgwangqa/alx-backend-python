f605b8fa28f437096ed3468efd36325637024378
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-07 01:56:48 +0200

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,9 +6,10 @@ import asyncio
 from typing import List
 from 0-basic_async_syntax import wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Asynchronously spawn `n` instances of `wait_random` and return the delays in ascending order.
+    Asynchronously spawn `n` instances of `wait_random` and return the delays.
     """
     delays = await asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
     return sorted(delays)
