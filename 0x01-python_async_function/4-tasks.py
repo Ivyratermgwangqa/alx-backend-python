be082eaeaf341952271dbbb6b6d1717dc24e5fce
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-07 01:40:20 +0200

    Update 4-tasks.py

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module that defines a function to create an asyncio task
+"""
+import asyncio
+from 0-basic_async_syntax import wait_random
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Returns an asyncio task wrapping `wait_random`.
+    """
+    return asyncio.create_task(wait_random(max_delay))
