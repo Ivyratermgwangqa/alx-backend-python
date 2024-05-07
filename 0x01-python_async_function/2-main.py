3339e304ef1eb6957b6570a4558e56dc265e7ed3
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-07 03:37:00 +0200

    Update 2-main.py

--- 0x01-python_async_function/2-main.py
+++ 0x01-python_async_function/2-main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
+import importlib
 import sys
+import time
+
+# Ensure the current directory is in sys.path
 sys.path.append('.')
 
-from 2-measure_runtime import measure_time
+# Use importlib to import the module with a string
+module_name = "2-measure_runtime"
+imported_module = importlib.import_module(module_name)
+
+# Access the measure_time function from the imported module
+measure_time = imported_module.measure_time
 
 print(measure_time(5, 9))
