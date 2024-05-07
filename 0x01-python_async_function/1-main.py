3da56aaca1fe0459821666c147e99cc674dc89b4
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-07 04:12:40 +0200

    Update 1-main.py

--- 0x01-python_async_function/1-main.py
+++ 0x01-python_async_function/1-main.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 import asyncio
-import sys
-sys.path.append('.')
+from 1-concurrent_coroutines import wait_n  # Import `wait_n` from `1-concurrent_coroutines`
 
-wait_n = __import__('1-concurrent_coroutines').wait_n
-
-print(asyncio.run(wait_n(5, 5)))
+print(asyncio.run(wait_n(5, 5)))  # Example test
 print(asyncio.run(wait_n(10, 7)))
 print(asyncio.run(wait_n(10, 0)))
