b6dab352c69e744e3af18b833cd201c870a085ac
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-07 03:41:11 +0200

    Update 4-main.py

--- 0x01-python_async_function/4-main.py
+++ 0x01-python_async_function/4-main.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
+"""
+Test script for task_wait_n function.
+"""
+import importlib
 import asyncio
 import sys
+
+# Ensure the current directory is in `sys.path`
 sys.path.append('.')
 
-from 4-tasks import task_wait_n
+# Use `importlib` to import `4-tasks`
+module_name = "4-tasks"
+imported_module = importlib.import_module(module_name)
 
+# Get `task_wait_n` from the imported module
+task_wait_n = imported_module.task_wait_n
 
+# Test with 5 tasks and a maximum delay of 6 seconds
 n = 5
 max_delay = 6
 print(asyncio.run(task_wait_n(n, max_delay)))
